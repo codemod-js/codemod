312e4efa6bb59025c4ca0165a49bf0eb1330a736
Brian Donovan <me@brian-donovan.com>
2018-03-20 07:58:56 -0700

    fix: use all syntax plugins
    When transpiling required files we need to be pretty liberal about what syntax we allow, otherwise we can end up in a situation where the current node version can actually parse more natively than babylon is allowed to by `@babel/preset-env`. This should fix that.
    
    Fixes #102

---
+++ src/AllSyntaxPlugin.ts
@@ -0,0 +1,28 @@
+import * as Babel from '@babel/core';
+import { BabelOptions, ParseOptions } from './TransformRunner';
+
+export const ALL_PLUGINS = [
+  'flow',
+  'jsx',
+  'asyncGenerators',
+  'classProperties',
+  'doExpressions',
+  'exportExtensions',
+  'functionBind',
+  'functionSent',
+  'objectRestSpread',
+  'dynamicImport',
+  'decorators'
+];
+
+export default function(babel: typeof Babel) {
+  return {
+    manipulateOptions(opts: BabelOptions, parserOpts: ParseOptions) {
+      for (let plugin of ALL_PLUGINS) {
+        if (plugin !== 'flow' || !opts.filename.endsWith('.ts')) {
+          parserOpts.plugins.push(plugin);
+        }
+      }
+    }
+  };
+}
