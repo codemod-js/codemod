89d514aba5b0e7b34771fdfe96a09ef59abbb706
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2018-12-10 09:14:36 -0800

    chore(package): hoist scripts up to the root

---
+++ script/_utils/run-command.js
@@ -0,0 +1,23 @@
+module.exports = function(name) {
+  new Promise(resolve => {
+    require('ts-node').register({
+      compilerOptions: {
+        lib: ['esnext'],
+        target: 'esnext'
+      }
+    });
+
+    const main = require(name).default;
+
+    resolve(main(
+      process.argv.slice(2),
+      process.stdin,
+      process.stdout,
+      process.stderr
+    ));
+  }).then(status => process.exit(status))
+    .catch(error => {
+      process.stderr.write(`[CRASH] ${error.stack}\n`);
+      process.exit(1);
+    });
+};
