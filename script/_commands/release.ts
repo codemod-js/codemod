727d7da78edf53893af0b9ecf6bdd5179a2220b8
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-01-03 14:50:43 -0800

    Revert "Merge pull request #193 from codemod-js/semantic-release-monorepo"
    This reverts commit b58704cc00e099ed320aabda197be292a4e30697, reversing
    changes made to 23839c9829ec959dce32501f701ebe7f7c93e096.

--- script/_commands/release.ts
+++ script/_commands/release.ts
@@ -1,5 +1,4 @@
 import runInPackages from '../_utils/runInPackages';
-import runInDirectory from '../_utils/runInDirectory';
 
 export default async function main(
   args: Array<string>,
@@ -7,19 +6,11 @@ export default async function main(
   stdout: NodeJS.WriteStream,
   stderr: NodeJS.WriteStream
 ): Promise<void> {
-  await runInDirectory(
-    stdin,
-    stdout,
-    stderr,
-    '.',
-    'npm',
-    ['install', '-g', 'semantic-release', 'semantic-release-monorepo']
-  );
   await runInPackages(
     stdin,
     stdout,
     stderr,
-    'semantic-release',
-    ['-e', 'semantic-release-monorepo']
+    'npx',
+    ['semantic-release', '-e', 'semantic-release-monorepo']
   );
 }
