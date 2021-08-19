283b26bc5824218506b9b596c500398ddfbb4992
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-08-19 10:54:29 -0700

    fix(parser): support babel with "hack"-style pipes
    In babel v7.15.0 the "hack"-style pipeline operator was added which made the "smart" proposal deprecated. Additionally, there's now conflict between "smart"/"hack" pipes and "hash"-style records & tuples.
    
    So now we default to the "minimal" pipeline operator proposal and ensure we don't add any conflicting plugins to an existing list.
    
    Refs https://github.com/decaffeinate/decaffeinate/issues/2407

--- packages/parser/src/options.ts
+++ packages/parser/src/options.ts
@@ -1,19 +1,18 @@
 import {
+  DecoratorsPluginOptions,
+  FlowPluginOptions,
   ParserOptions as BabelParserOptions,
-  ParserPlugin as BabelParserPlugin,
+  ParserPlugin,
+  PipelineOperatorPluginOptions,
+  RecordAndTuplePluginOptions,
+  TypeScriptPluginOptions,
 } from '@babel/parser'
-
-/**
- * Add some type plugins that are in master but not yet published.
- */
-export type ParserPlugin =
-  | BabelParserPlugin
-  | 'importAssertions'
-  | ['recordAndTuple', { syntaxType: 'hash' | 'bar' }]
 
 type ParserPluginName =
   | Extract<ParserPlugin, string>
   | Extract<ParserPlugin, [string, object]>[0]
+
+type ParserPluginOptions = Extract<ParserPlugin, [string, object]>[1]
 
 const DefaultParserPlugins = new Set<ParserPlugin>([
   'asyncGenerators',
@@ -41,7 +40,7 @@ const DefaultParserPlugins = new Set<ParserPlugin>([
   'throwExpressions',
   'topLevelAwait',
   ['decorators', { decoratorsBeforeExport: true }],
-  ['pipelineOperator', { proposal: 'smart' }],
+  ['pipelineOperator', { proposal: 'minimal' }],
   ['recordAndTuple', { syntaxType: 'hash' }],
 ])
 
@@ -65,7 +64,7 @@ export default function buildOptions({
   ...rest
 }: ParserOptions = {}): ParserOptions {
   for (const plugin of DefaultParserPlugins) {
-    if (shouldAddPlugin(plugins, getPluginName(plugin))) {
+    if (shouldAddPlugin(plugins, plugin)) {
       plugins = [...plugins, plugin]
     }
   }
@@ -90,33 +89,6 @@ export default function buildOptions({
 }
 
 /**
- * Gets plugins that cannot be enabled if the plugin given by `name` is enabled.
- *
- * @see https://github.com/babel/babel/blob/5fb4d84a33351c13057dc542513a3fe2309e08b0/packages/babel-parser/src/plugin-utils.js#L44-L49
- */
-function getMutuallyExclusivePluginsForPlugin(
-  name: ParserPluginName
-): ReadonlyArray<ParserPluginName> {
-  switch (name) {
-    case 'flow':
-    case 'flowComments':
-      return ['typescript']
-
-    case 'typescript':
-      return ['flow', 'flowComments']
-
-    case 'decorators':
-      return ['decorators-legacy']
-
-    case 'decorators-legacy':
-      return ['decorators']
-
-    default:
-      return []
-  }
-}
-
-/**
  * Gets the type plugin to use for a given file name.
  *
  * @example
@@ -134,6 +106,45 @@ function typePluginForSourceFileName(
   }
 }
 
+function getPluginOptions(
+  plugins: Array<ParserPlugin>,
+  name: 'decorators'
+): DecoratorsPluginOptions | undefined
+function getPluginOptions(
+  plugins: Array<ParserPlugin>,
+  name: 'pipelineOperator'
+): PipelineOperatorPluginOptions | undefined
+function getPluginOptions(
+  plugins: Array<ParserPlugin>,
+  name: 'recordAndTuple'
+): RecordAndTuplePluginOptions | undefined
+function getPluginOptions(
+  plugins: Array<ParserPlugin>,
+  name: 'flow'
+): FlowPluginOptions | undefined
+function getPluginOptions(
+  plugins: Array<ParserPlugin>,
+  name: 'typescript'
+): TypeScriptPluginOptions | undefined
+function getPluginOptions(
+  plugins: Array<ParserPlugin>,
+  name: ParserPluginName
+): ParserPluginOptions | undefined
+function getPluginOptions(
+  plugins: Array<ParserPlugin>,
+  name: ParserPluginName
+): ParserPluginOptions | undefined {
+  for (const plugin of plugins) {
+    if (Array.isArray(plugin)) {
+      if (plugin[0] === name) {
+        return plugin[1]
+      }
+    } else if (plugin === name) {
+      return {}
+    }
+  }
+}
+
 /**
  * Determines whether a plugin list can accept a new plugin by name.
  *
@@ -145,17 +156,63 @@ function typePluginForSourceFileName(
  */
 function shouldAddPlugin(
   plugins: Array<ParserPlugin>,
-  name: ParserPluginName
+  plugin: ParserPlugin
 ): boolean {
+  const name = getPluginName(plugin)
+
   if (pluginListIncludesPlugin(plugins, name)) {
     return false
   }
 
-  return !getMutuallyExclusivePluginsForPlugin(
-    name
-  ).some((mutuallyExclusivePlugin) =>
-    pluginListIncludesPlugin(plugins, mutuallyExclusivePlugin)
-  )
+  switch (name) {
+    case 'flow':
+    case 'flowComments':
+      return !getPluginOptions(plugins, 'typescript')
+
+    case 'typescript':
+      return !(
+        getPluginOptions(plugins, 'flow') ||
+        getPluginOptions(plugins, 'flowComments')
+      )
+
+    case 'decorators':
+      return !getPluginOptions(plugins, 'decorators-legacy')
+
+    case 'decorators-legacy':
+      return !getPluginOptions(plugins, 'decorators')
+
+    case 'recordAndTuple':
+    case 'pipelineOperator': {
+      const recordAndTupleOptions =
+        name === 'recordAndTuple'
+          ? (plugin[1] as RecordAndTuplePluginOptions)
+          : getPluginOptions(plugins, 'recordAndTuple')
+      const pipelineOperatorOptions =
+        name === 'pipelineOperator'
+          ? (plugin[1] as PipelineOperatorPluginOptions)
+          : getPluginOptions(plugins, 'pipelineOperator')
+
+      if (recordAndTupleOptions?.syntaxType === 'hash') {
+        // https://github.com/babel/babel/blob/15f2f171ab13b224757ca43483a456e409f12a0a/packages/babel-parser/src/plugin-utils.js#L124-L128
+        if (pipelineOperatorOptions?.proposal === 'smart') {
+          return false
+        }
+
+        // https://github.com/babel/babel/blob/15f2f171ab13b224757ca43483a456e409f12a0a/packages/babel-parser/src/plugin-utils.js#L119-L123
+        if (
+          pipelineOperatorOptions?.proposal === 'hack' &&
+          pipelineOperatorOptions.topicToken === '#'
+        ) {
+          return false
+        }
+      }
+
+      return true
+    }
+
+    default:
+      return true
+  }
 }
 
 /**
@@ -182,6 +239,6 @@ function pluginListIncludesPlugin(
  *   getPluginName('decorators');            // 'decorators'
  *   getPluginName(['flow', { all: true }]); // 'flow'
  */
-function getPluginName(plugin: ParserPlugin): ParserPluginName {
+export function getPluginName(plugin: ParserPlugin): ParserPluginName {
   return typeof plugin === 'string' ? plugin : plugin[0]
 }
