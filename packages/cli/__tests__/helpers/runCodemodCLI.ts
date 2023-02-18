1d6f1029bdbc309f36c4799c083dda636256e8c2
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-02-18 11:40:31 -0800

    test: use the `NODE` environment variable if available

--- packages/cli/__tests__/helpers/runCodemodCLI.ts
+++ packages/cli/__tests__/helpers/runCodemodCLI.ts
@@ -11,16 +11,8 @@ export async function runCodemodCLI(
   args: Array<string>,
   { stdin = '', cwd }: { stdin?: string; cwd?: string } = {}
 ): Promise<CLIResult> {
-  console.log('runCodemodCLI', {
-    args,
-    stdin,
-    cwd,
-    argv0: process.argv0,
-    __dirname,
-    codemodPath: join(__dirname, '../../bin/codemod'),
-  })
   const child = spawn(
-    process.argv0,
+    process.env.NODE ?? process.argv0,
     [join(__dirname, '../../bin/codemod'), ...args],
     {
       stdio: 'pipe',
